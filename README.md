ecfb9465402178aa58c675f02780ab7fe3994cff
zac-williamson <blorktronics@gmail.com>
2024-07-26 23:03:28 -0400

    updated readme

--- README.md
+++ README.md
@@ -8,25 +8,35 @@ NOTE: library is experimental and currently will only compile using the nightly 
 
 # Usage
 
-See tests in `lib.nr` for examples. (TODO: flesh this out!)
+See tests in `lib.nr` for examples.
 
-```
+To construct a `BigNumInstance` objects, both the bignum modulus (the public key) and a Barrett reduction parameter are required as arrays of Field elements, with each element representing a 120-bit slice of the number.
+
+See `signature_gen/src/main.rs` for how these parameters can be derived. The rust crate `noir-bignum-paramgen` contains both libraries and an executable that performs this formatting.
+
+## Example Use
+
+See tests in `lib.nr` for additional examples. The `pubkey_redc_param` parameter can be derived via the `noir-bignum-paramgen` tool and provided as a witness via Prover.toml
+
+```rust
+    use dep::noir_rsa::bignum::BigNum;
+    use dep::noir_rsa::bignum::runtime_bignum::BigNumInstance;
+    use dep::noir_rsa::bignum::fields::Params2048;
+    use dep::noir_rsa::RSA;
+
+    type BN2048 = BigNum<18, Params2048>;
+    type BNInstance = BigNumInstance<18, Params2048>;
+    type RSA2048 = RSA<BN2048, BNInstance, 256>;
+
+    fn verify_signature(pubkey: [u8; 256], signature: [u8; 256], pubkey_redc_param: BN2048)
     let sha256_hash: [u8; 32] = dep::std::hash::sha256("hello world".as_bytes());
-    let BNInstance: BigNumInstance<18, Params2048> = BigNumInstance::new(
-        [ // first parameter = modulus
-        0x183b4cadacea64716b6535b25a86ad, 0x129a32e9fbd374949e81b080546c2b, 0x9276332765f54b9b4d6fe23f4983a4, 0x91df05fde1a414c9fbf38808a01032, 0x97ca171c382e082ddd6c81be090902, 0x97161dde47ed979939451704b72648, 0x3b66f0c93f8ddbc62bb99f6b8fad2d, 0xc3a046fcad814d8938da372cd84d2a, 0xfe903cfe3ee4de6de3916023bb87f2, 0x8be5fd99c8b46dce9aab27ce4b3883, 0x0d99dfe50dd2288123804872da8543, 0xbb6d8afef8b2c7e3b86ad96973d4e5, 0x17e08fa3c5391279b844581f67825c, 0x09511f4a436abe495bfbfcc254d9c5, 0xf80d02572d620eab5ad7ecfbe2c20c, 0x2a5307402a05f59ba232ad0c8366d0, 0x7b9169630b317c38d549a86f85fcfc, 0xf2
-    ],
-        [ // second parameter = barrett reduction parameter (used in unconstrained functions, does not need to be constrained or derived in-circuit from modulus)
-        0xf4effa025abe90db41dc5bb5934a5f, 0x2596b25aa09f6a0e290accebbef006, 0x330e9567ec5eca92b7a8ce72ad3d83, 0xccf98d178ec4017e4947cadc02da7c, 0xaed6044ffeeb12de4d1e67aedfda78, 0xb5ae8a5a4637b632800e2f41dfbf0a, 0x8304360d359cf2f0d5d97c039f9b04, 0x53cde0d0633c0e66e1da9a118f6a96, 0xf9a83822351097bb1ed123d10290f5, 0xeb489c7f7227c09ca0117264dcf04f, 0x6a1b3618478a235f438adf84533177, 0x71c03085e38a87f5ec8e51c27dde98, 0xcf3e89d9cf253ef61d65d4431334d9, 0xa5a1a59e98fb7f49c746149324201d, 0x38f46c635f884f24c254eaa37981d6, 0x1c42be15dd95e1ac5bf01972b24ff9, 0x4555dd506d1f89e61279293c03db90, 0x010e
-    ]
-    );
-    let signature = BNInstance.from_array(
-        [
-        0x5f0937ed1aacbdb83991e21c89ca8a, 0x5a1fa4ef2faf4042401c9121c73874, 0x7379fbb7713fbf807d250f7401afb8, 0x9cc5ce8813f3a83a72b681a4d434b2, 0x11cb49ac2ee1f9bed8a008b8327e1e, 0xf3362524f1224e48827b34a62f6ace, 0x3498a2944e32a3798fe9da684b32ae, 0xc346c33528bcdf7a06805126a29402, 0x6ac93f2fa68f270ec73674eae24380, 0x222c72de080496533ceeb8af827910, 0xc22889b311a53203278b942ca67100, 0x93d75567f7fe9ee7ca9871a0cb09f9, 0x049dabd976a9574847b1614f6b739c, 0xdc66b621dbae623df9b657b52fa1b0, 0xf37e82cc1eced382e11fc32e673f67, 0xe50545eb9027f1144a0ba7bbb0c886, 0xb6193409539c515d3c55a45c5734b1, 0x4b
-    ]
-    );
-    let empty_array: [u8; 256] = [0; 256];
-    assert(verify_sha256_pkcs1v15(BNInstance, sha256_hash, signature, empty_array));
+    let modulus: BN2048 = BigNum::from_byte_be(pubkey);
+    let signature: BN2048 = BigNum::from_byte_be(signature);
+
+    let instance: BNInstance = BigNumInstance::new(modulus, pubkey_redc_param);
+
+    let rsa: RSA2048 = RSA {};
+    assert(rsa.verify_sha256_pkcs1v15(BNInstance, sha256_hash, signature));
 ```
 
 # Costs
--- signature_gen/src/main.rs
+++ signature_gen/src/main.rs
@@ -1,110 +1,22 @@
- use rsa::{Pkcs1v15Encrypt, RsaPrivateKey, RsaPublicKey, pkcs8, signature};
- use rsa::pkcs1v15::VerifyingKey;
- use rsa::pkcs1v15::Signature;
- use num_bigint::BigUint;
- use base64ct::{Base64, Encoding};
- use hex_literal::hex;
- use num_traits::FromPrimitive;
- use num_traits::ToPrimitive;
- use num_traits::Num;
+use num_bigint::BigUint;
+use rsa::pkcs1v15::Signature;
+use rsa::pkcs1v15::VerifyingKey;
+use rsa::{RsaPrivateKey, RsaPublicKey};
 
- use rsa::errors::{Error, Result};
- use rsa::Pkcs1v15Sign;
- use sha1::{Digest, Sha1};
- use sha2::Sha256;
- use rsa::traits::{
-    Decryptor, EncryptingKeypair, PublicKeyParts, RandomizedDecryptor, RandomizedEncryptor,
+use rsa::traits::PublicKeyParts;
+use sha2::Sha256;
+use rsa::signature::{
+    SignatureEncoding, Signer, Verifier,
 };
-
 use rand;
 
-const NUM_BITS: u64 = 2048;
-const BITS_PER_LIMB: u64 = 120;
-const NUM_LIMBS: u64 = (NUM_BITS / BITS_PER_LIMB) + ((NUM_BITS % BITS_PER_LIMB != 0) as u64);
-
-
-fn get_modtest() -> BigUint {
-
-    let limbs: [&str; 9] =
-    ["e908724e0d28a1565511879e00f063", "58dea28d5112982c8ab8597d3e611f", "b115f9878acc910a47b41ac8d5f763", "c71b0a719c2283f710f86024737cee", "9ee9eb73e95b84f033af5217337427", "8fcab34c99cc2661dd57de15ae2110", "a38db8a829eec5550342f6f66334dc", "d7c4b32fd351a47d860fda1897330e", "98c92a33a0f33fd7"];
-    let shift: BigUint = (BigUint::from(1 as u64) << BITS_PER_LIMB as usize);
-    let mut result: BigUint = BigUint::from(0 as u64);
-    for i in 0..9 {
-        let idx = 8 - i;
-        let limbstr = limbs[idx as usize];
-        let r = BigUint::from_str_radix(limbstr, 16);
-        let limb: BigUint = r.expect("uh oh?");
-    
-        result = result.clone() << BITS_PER_LIMB as usize;
-        result = result.clone() + limb;
-    }
-    result
-}
-
-fn format_bignum(input: &[u8], msg: &str) {
-    const NUM_LIMBS: u64 = NUM_BITS / BITS_PER_LIMB + (NUM_BITS % BITS_PER_LIMB != 0) as u64;
-    let mut limbs: [[u8; 15]; NUM_LIMBS as usize] = [[0; 15]; NUM_LIMBS as usize];
-
-    for i in 0..input.len() {
-        let limb_num = i / 15;
-        let limb_idx = i % 15;
-        // internal limbs are big endian
-        limbs[limb_num][14 - limb_idx] = input[i];
-    }
-    print!("{} = [", msg);
-    for i in 0..NUM_LIMBS - 1 {
-        print!("0x{}, ", hex::encode(&limbs[i as usize]));
-    }
-    println!("0x{}]", hex::encode(&limbs[NUM_LIMBS as usize - 1]));
-}
-
-fn format_bignum2(input: Vec<BigUint>, msg: &str) {
-
-    print!("{}, [", msg);
-    for i in 0..input.len() - 1 {
-        let bytes = input[i].to_bytes_be();
-        print!("0x{}, ", hex::encode(&bytes));
-    }
-    let bytes = input[input.len() - 1].to_bytes_be();
-    println!("0x{}]", hex::encode(&bytes));
-}
-
-fn split_into_limbs(mut input: BigUint) -> Vec<BigUint> {
-    let one = BigUint::from(1 as u64);
-    let mask: BigUint = (one.clone() << BITS_PER_LIMB as usize) - one.clone();
-
-    let mut r: Vec<BigUint> = Vec::new();
-    for _ in 0..NUM_LIMBS {
-        let slice = input.clone() & mask.clone();
-        input = input.clone() >> BITS_PER_LIMB as usize;
-        r.push(slice);
-    }
-    r
-}
-
-fn compute_double_modulus(modulus: BigUint) -> Vec<BigUint> {
-    let double_modulus = modulus.clone() + modulus.clone();
-
-    let shift = BigUint::from(1 as u64) << 120;
-    let mut limbs = split_into_limbs(double_modulus);
-    let num_limbs = limbs.len();
-    limbs[0] += shift.clone();
-    for i in 1..num_limbs - 1 {
-        limbs[i] = limbs[i].clone() - BigUint::from(1 as u64) + shift.clone();
-    }
-    limbs[num_limbs - 1] = limbs[num_limbs - 1].clone() - BigUint::from(1 as u64);
-    limbs
-}
+use noir_bignum_paramgen::{bn_limbs, bn_runtime_instance};
 
 fn generate_2048_bit_signature_parameters() {
     let mut rng: rand::prelude::ThreadRng = rand::thread_rng();
     let bits: usize = 2048;
     let priv_key = RsaPrivateKey::new(&mut rng, bits).expect("failed to generate a key");
     let pub_key: RsaPublicKey = priv_key.clone().into();
-
-    
-    let multiplicand = BigUint::new([1].to_vec()) << 4096;
-    let barrett_reduction_parameter =  multiplicand / pub_key.n();
 
     let msg: &str = "hello world";
 
@@ -114,188 +26,42 @@ fn generate_2048_bit_signature_parameters() {
     let sig_bytes = &Signature::try_from(sig.as_slice()).unwrap().to_bytes();
 
     let sig_uint: BigUint = BigUint::from_bytes_be(sig_bytes);
-    let sig_limbs = split_into_limbs(sig_uint.clone());
 
-    let modulus_limbs = split_into_limbs(pub_key.n().clone());
-    let double_modulus_limbs = compute_double_modulus(pub_key.n().clone());
-    let redc_limbs = split_into_limbs(barrett_reduction_parameter.clone());
 
-    format_bignum2(modulus_limbs, "modulus_2048: [Field; 18] =");
-    format_bignum2(double_modulus_limbs, "double_modulus: [Field; 18] = ");
-    format_bignum2(redc_limbs, "redc_param: [Field; 18] = ");
-    format_bignum2(sig_limbs, "signature: [Field; 18] = ");
+    let sig_str = bn_limbs(sig_uint.clone(), 2048);
+    println!("let signature: BigNum<18, Params2048> = BigNum::from_array({});", sig_str.as_str());
 
-    // format_bignum(&pub_key.n().to_bytes_le(), "modulus");
-    // format_bignum(&barrett_reduction_parameter.to_bytes_le(), "redc_param");
-  //  println!("signature bytes = {:?}", sig_bytes);
+    let r = bn_runtime_instance(pub_key.n().clone(), 2048, String::from("BNInstance"));
+    println!("{}", r.as_str());
 }
- fn main() {
+
+
+fn main() {
     generate_2048_bit_signature_parameters();
+}
 
-    // let modulus = get_modtest();
-    // let modulus_limbs = split_into_limbs(modulus.clone());
 
-    // let multiplicand = BigUint::new([1].to_vec()) << 2048;
-    // let barrett_reduction_parameter =  multiplicand / modulus.clone();
 
-    // let double_modulus_limbs = compute_double_modulus(modulus.clone());
-    // let redc_limbs = split_into_limbs(barrett_reduction_parameter.clone());
-
-    // format_bignum2(modulus_limbs, "modulus");
-    // format_bignum2(double_modulus_limbs, "double_modulus");
-    // format_bignum2(redc_limbs, "redc_param");
- }
-
- fn testing() {
-     let mut rng = rand::thread_rng();
-     let bits = 2048;
-     let priv_key = RsaPrivateKey::new(&mut rng, bits).expect("failed to generate a key");
-     let pub_key: RsaPublicKey = priv_key.clone().into();
-     println!("pub key = {:x?}", pub_key);
-     let foobar = pub_key.n().to_str_radix(16);
-
-     let pubkey_n: &BigUint = pub_key.n();
-     let mulx = BigUint::new([1].to_vec()) << 4096;
-     let r =  mulx / pubkey_n;
-     println!("test mul = {:?}", r.to_str_radix(16));
-    // pubkey_n.mul()
-     let text: &str = "hello world";
-     println!("pubkey foobar = {:?}", foobar);
-
-     
-     let signing_key = rsa::pkcs1v15::SigningKey::<Sha256>::new(priv_key);
+fn test_signature_generation_impl() {
+    let mut rng = rand::thread_rng();
+    let bits = 2048;
+    let priv_key = RsaPrivateKey::new(&mut rng, bits).expect("failed to generate a key");
+    let pub_key: RsaPublicKey = priv_key.clone().into();
+    let text: &str = "hello world";
+    let signing_key = rsa::pkcs1v15::SigningKey::<Sha256>::new(priv_key);
     let sig: Vec<u8> = signing_key.sign(text.as_bytes()).to_vec();
     let verifying_key = VerifyingKey::<Sha256>::new(pub_key);
 
-//     let bytes: &[u8] = b"rsa4096"; // HACK - the criterion is that the signature has leading zeros.
-//     let signature = signing_key.sign(bytes);
-    let foo = Pkcs1v15Sign::new::<Sha256>();
     let result = verifying_key.verify(
         text.as_bytes(),
         &Signature::try_from(sig.as_slice()).unwrap(),
     );
-    println!("sig? {:x?}", &Signature::try_from(sig.as_slice()).unwrap());
-    println!("errm {:?}", result);
     result.expect("failed to verify");
 
-//     let result = pub_key.verify(foo, &digest, sig);
-
-    // let pub_key = RsaPublicKey::from(&priv_key);
-    
-    // // Encrypt
-    // let data = b"hello world";
-    // let enc_data = pub_key.encrypt(&mut rng, Pkcs1v15Encrypt, &data[..]).expect("failed to encrypt");
-    // assert_ne!(&data[..], &enc_data[..]);
-    
-    // // Decrypt
-    // let dec_data = priv_key.decrypt(Pkcs1v15Encrypt, &enc_data).expect("failed to decrypt");
-    // assert_eq!(&data[..], &dec_data[..]);
-    test_verify_pkcs1v15();
-    println!("Hello, world!");
-    // println!("dec_data {:?}", dec_data);
 }
 
-// mod tests {
-    use rsa::*;
-    use rsa::signature::{
-        hazmat::{PrehashSigner, PrehashVerifier},
-        DigestSigner, DigestVerifier, Keypair, RandomizedDigestSigner, RandomizedSigner,
-        SignatureEncoding, Signer, Verifier,
-    };
-    use rand_chacha::{
-        rand_core::{RngCore, SeedableRng},
-        ChaCha8Rng,
-    };
-    use sha3::Sha3_256;
-    
-  //  use rsa::RsaPublicKey;
-    
-
-
-fn get_private_key() -> RsaPrivateKey {
-    // In order to generate new test vectors you'll need the PEM form of this key:
-    // -----BEGIN RSA PRIVATE KEY-----
-    // MIIBOgIBAAJBALKZD0nEffqM1ACuak0bijtqE2QrI/KLADv7l3kK3ppMyCuLKoF0
-    // fd7Ai2KW5ToIwzFofvJcS/STa6HA5gQenRUCAwEAAQJBAIq9amn00aS0h/CrjXqu
-    // /ThglAXJmZhOMPVn4eiu7/ROixi9sex436MaVeMqSNf7Ex9a8fRNfWss7Sqd9eWu
-    // RTUCIQDasvGASLqmjeffBNLTXV2A5g4t+kLVCpsEIZAycV5GswIhANEPLmax0ME/
-    // EO+ZJ79TJKN5yiGBRsv5yvx5UiHxajEXAiAhAol5N4EUyq6I9w1rYdhPMGpLfk7A
-    // IU2snfRJ6Nq2CQIgFrPsWRCkV+gOYcajD17rEqmuLrdIRexpg8N1DOSXoJ8CIGlS
-    // tAboUGBxTDq3ZroNism3DaMIbKPyYrAqhKov1h5V
-    // -----END RSA PRIVATE KEY-----
-
-    RsaPrivateKey::from_components(
-        BigUint::from_str_radix("9353930466774385905609975137998169297361893554149986716853295022578535724979677252958524466350471210367835187480748268864277464700638583474144061408845077", 10).unwrap(),
-        BigUint::from_u64(65537).unwrap(),
-        BigUint::from_str_radix("7266398431328116344057699379749222532279343923819063639497049039389899328538543087657733766554155839834519529439851673014800261285757759040931985506583861", 10).unwrap(),
-        vec![
-            BigUint::from_str_radix("98920366548084643601728869055592650835572950932266967461790948584315647051443",10).unwrap(),
-            BigUint::from_str_radix("94560208308847015747498523884063394671606671904944666360068158221458669711639", 10).unwrap()
-        ],
-    ).unwrap()
+#[test]
+fn test_signature_generation()
+{
+    test_signature_generation_impl();
 }
-
-fn test_verify_pkcs1v15() {
-    let priv_key = get_private_key();
-
-    let tests = [
-        (
-            "Test.\n",
-            hex!(
-                "a4f3fa6ea93bcdd0c57be020c1193ecbfd6f200a3d95c409769b029578fa0e33"
-                "6ad9a347600e40d3ae823b8c7e6bad88cc07c1d54c3a1523cbbb6d58efc362ae"
-            ),
-            true,
-        ),
-        (
-            "Test.\n",
-            hex!(
-                "a4f3fa6ea93bcdd0c57be020c1193ecbfd6f200a3d95c409769b029578fa0e33"
-                "6ad9a347600e40d3ae823b8c7e6bad88cc07c1d54c3a1523cbbb6d58efc362af"
-            ),
-            false,
-        ),
-    ];
-    let pub_key: RsaPublicKey = priv_key.into();
-
-    for (text, sig, expected) in &tests {
-        let digest = Sha1::digest(text.as_bytes()).to_vec();
-        let result = pub_key.verify(Pkcs1v15Sign::new::<Sha1>(), &digest, sig);
-        match expected {
-            true => result.expect("failed to verify"),
-            false => {
-                result.expect_err("expected verifying error");
-            }
-        }
-    }
-}
-// }
-
-// fn sign_verify() {
-//     use pkcs8::DecodePrivateKey;
-//     use signature::Signer;
-
-//     use rsa::pkcs1v15::SigningKey;
-//     use rsa::pkcs1v15::VerifyingKey;
-
-//     use rsa::RsaPrivateKey;
-//     let mut rng = rand::thread_rng();
-//     let bits = 2048;
-
-//     let priv_key = RsaPrivateKey::new(&mut rng, bits).expect("failed to generate a key");
-//     let pub_key = RsaPublicKey::from(&priv_key);
-
-//     let signing_key = SigningKey::<sha2::Sha256>::new(priv_key);
-
-//     let bytes: &[u8] = b"rsa4096"; // HACK - the criterion is that the signature has leading zeros.
-//     let signature = signing_key.sign(bytes);
-
-//     let verifying_key = VerifyingKey::<sha2::Sha256>::new(pub_key);
-
-//     verifying_key.verify();
-//     // let expected: &str = "029E365B60971D5A499FF5E1C288B954D3A5DCF52482CEE46DB90DC860B725A8D6CA031146FA156E9F17579BE6122FFB11DAC35E59B2193D75F7B31CE1442DDE7F4FF7885AD5D6080266E9A33BB4CEC93FCC2B6B885457A0ABF19E2DAA00876F694B37F535F119925CCCF9A17B90AE6CF39F07D7FEFBEECDF1B344C14B728196DDD154230BADDEDA5A7EFF373F6CD3EF6D41789572A7A068E3A252D3B7D5D706C6170D8CFDB48C8E738A4B3BFEA3E15716805E376EBD99EA09C6E82F3CFA13CEB23CD289E8F95C27F489ADC05AAACE8A9276EE7CED3B7A5C7264F0D34FF18CEDC3E91D667FCF9992A8CFDE8562F65FDDE1E06595C27E0F82063839A358C927B2";
-//     // assert_eq!(format!("{}", signature), expected);
-//     // assert_eq!(format!("{:x}", signature), expected.to_lowercase());
-//     // assert_eq!(format!("{:X}", signature), expected);
-//     // assert_eq!(signature.to_string(), expected);
-// }
